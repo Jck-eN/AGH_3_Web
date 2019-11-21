65047f2e5b8c126ef218a5153b55cfa19cd9d31a
Jck-eN <jaceknit.rac@gmail.com>
2019-11-21 20:50:02 +0100

    added lab3

--- lab3/js/scripts.js
+++ lab3/js/scripts.js
@@ -2,9 +2,9 @@ var cards = document.querySelectorAll(".single-card");
 cards.forEach(card => {
     card.addEventListener("mouseover", function(e) {
 
-        console.log("over");
+        console.log("in");
         var recipe = card.querySelector(".card-bottom");
-        recipe.style.height = "300px";
+        recipe.style.height = "65px";
     })
     card.addEventListener("mouseout", function(e) {
         var e = event.toElement || event.relatedTarget;
@@ -21,5 +21,33 @@ cards.forEach(card => {
 
     button.addEventListener("click", () => {
         card.querySelector(".card-desciption-additional").style.display = "block";
+        var recipe = card.querySelector(".card-bottom");
+        recipe.style.height = "300px";
     })
 });
+
+
+const scrollTopPageButton = document.getElementById('scroll-button');
+const scrolledPage = () => {
+    let y = window.scrollY;
+    if (y > 0) {
+        scrollTopPageButton.className = "top-scroll show";
+    } else {
+        scrollTopPageButton.className = "top-scroll hide";
+    }
+};
+
+window.addEventListener("scroll", scrolledPage);
+
+const scrollTopPage = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollTopPage);
+        window.scrollTo(0, c - c / 1.3);
+    }
+};
+
+scrollTopPageButton.onclick = function(e) {
+    e.preventDefault();
+    scrollTopPage();
+}
